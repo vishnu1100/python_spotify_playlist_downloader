aa55bbb8718d2b3e2471b5347bdd065129a54584
_VISHNU_ <vishnusanthoshvr@gmail.com>
2024-09-01 00:13:33 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,27 @@
+import subprocess
+
+def download_spotify_playlist(playlist_url, output_directory):
+    # Full path to spotdl executable
+    spotdl_path = r'C:\Users\Vishnu\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\LocalCache\local-packages\Python311\Scripts\spotdl.exe'
+
+    # Create the command to download the playlist
+    command = [
+        spotdl_path, '--playlist', playlist_url,
+        '--output', f'{output_directory}/%(title)s.%(ext)s'
+    ]
+
+    try:
+        # Run the command using subprocess
+        subprocess.run(command, check=True)
+        print("Download completed successfully!")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+    except FileNotFoundError:
+        print("Error: spotdl not found. Please ensure it's installed and in your PATH.")
+
+if __name__ == "__main__":
+    link = input("Paste spotify  playlist URL link: ")
+    playlist_url = link;
+    output_directory = r"C:\Users\Vishnu\Desktop\musik"
+
+    download_spotify_playlist(playlist_url, output_directory)
